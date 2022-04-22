485600f52f11dd01621dfea30431622a90c27e01
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-04-22 17:08:50 +0300

    Add func for grouping up files

--- butler.py
+++ butler.py
@@ -2,6 +2,9 @@ import argparse
 import os, shutil
 
 
+files_extension = [".txt", ".doc", ".docx", ".pdf", ".xlsx", ".bmp", ".jpg"]
+
+
 def clean_the_dir(directory_path):
     for files in os.listdir(directory_path):
         path = os.path.join(directory_path, files)
@@ -9,6 +12,8 @@ def clean_the_dir(directory_path):
             shutil.rmtree(path)
         except OSError:
             os.remove(path)
+
+# def is_dir_exist():
 
 
 parser = argparse.ArgumentParser(
@@ -20,13 +25,22 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('--clean', help="Clean trash can")
 
-# parser.add_argument('--dir', default="/tmp/", help="Dir to group up the files")
+parser.add_argument('--dir', help="Dir to group up the files")
 args = parser.parse_args()
 
 
 if args.clean:
     clean_the_dir(args.clean)
-# elif args.dir == '/tmp/':
-#     print('You group up it!')
-#     Group up dir
+elif args.dir:
+    for file in os.listdir(args.dir):
+        for ext in files_extension:
+            if file.endswith(ext):
+                print(os.path.join(args.dir, file))
+                print("ALL_" + ext.upper())
+                try:
+                    os.mkdir(args.dir + "ALL_" + ext.upper())
+                except OSError as os_error:
+                    print(os_error)
+                shutil.move(os.path.join(args.dir, file), args.dir + "ALL_" + ext.upper())
+
 
